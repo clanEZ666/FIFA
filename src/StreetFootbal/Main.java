002d2f32d9235e97587c5f993c84c68a9eeefbcc
ClanEZ <bernardosoto30rus@gmail.com>
2024-11-03 17:31:25 +0400

    DZ37-38

--- src/StreetFootbal/Main.java
+++ src/StreetFootbal/Main.java
@@ -1,4 +1,29 @@
 package StreetFootbal;
 
 public class Main {
+    public static void main(String[] args) {
+        Player p1 = new Player();
+        Player p2 = new Player();
+        Player p3 = new Player();
+        Player p4 = new Player();
+        Player p5 = new Player();
+        Player p6 = new Player();
+
+        Player.info();
+
+
+        Player p7 = new Player();
+        Player p8 = new Player();
+
+        Player.info();
+
+
+        while(p1.getStamina() > Player.MIN_STAMINA){
+            p1.run();
+        }
+
+        System.out.println("Игрок 1 выдохся полностью и покинул поле");
+        System.out.println("Текущее кол-во игроков на поле: " +Player.getCountPlayers());
+    }
+
 }
--- src/StreetFootbal/Player.java
+++ src/StreetFootbal/Player.java
@@ -1,7 +1,58 @@
 package StreetFootbal;
 
+import java.util.Random;
+
 public class Player {
 
+    private int stamina;
+
+    public static final int MAX_STAMINA = 100;
+    public static final int MIN_STAMINA = 0;
+
+    private static int countPlayers = 0;
+
+    private static final int MAX_PLAYERS_ON_FIELD = 6;
+
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public Player() {
+        Random rand = new Random();
+        this.stamina = 90 + rand.nextInt(11);
+
+        if(countPlayers < MAX_PLAYERS_ON_FIELD) {
+            countPlayers++;
+        }
+    }
+      //Метод изменения выносливоости на -1 при вызове
+    public void run() {
+        if (stamina > MIN_STAMINA) {
+            stamina--;
+
+            if (stamina == MIN_STAMINA) {
+                countPlayers--;
+            }
+
+        }
+
+    }
+
+    public static void info() {
+        if (countPlayers < MAX_PLAYERS_ON_FIELD) {
+            int freeSpots = MAX_STAMINA - countPlayers;
+            System.out.println("Команды неполные. На поле еще есть " + freeSpots + " свободных мест.");
+        } else {
+            System.out.println("На поле нету свободных мест");
+        }
+    }
+    public static int getCountPlayers() {
+        return countPlayers;
+    }
 
 
 }
+
+
+
