4655e3727950e54a26f3fd6acef437304c23724a
ClanEZ <bernardosoto30rus@gmail.com>
2024-11-18 01:57:00 +0400

    DZ41-42

--- src/StreetFootbal/Main.java
+++ src/StreetFootbal/Main.java
@@ -17,13 +17,42 @@ public class Main {
 
         Player.info();
 
+        Game game = new Game();
 
-        while(p1.getStamina() > Player.MIN_STAMINA){
+        game.addPlayer("Александр");
+        game.addPlayer("Владимир");
+        game.addPlayer("Максим");
+        game.addPlayer("Ярослав");
+        game.addPlayer("Мирослав");
+        game.addPlayer("Миша");
+
+        game.addPlayer("Олег");
+
+        game.displayPlayers();
+
+        while (p1.getStamina() > Player.MIN_STAMINA) {
             p1.run();
         }
 
         System.out.println("Игрок 1 выдохся полностью и покинул поле");
-        System.out.println("Текущее кол-во игроков на поле: " +Player.getCountPlayers());
+        System.out.println("Текущее кол-во игроков на поле: " + Player.getCountPlayers());
+
+
+        while (p1.getStamina() > Player.MIN_STAMINA) {
+            p1.run();
+        }
+        p1.setExhausted(true);
+
+        System.out.println("Игрок 1 выдохся полностью и покинул поле.");
+        System.out.println("Текущее кол-во игроков на поле: " + Player.getCountPlayers());
+
+
+        game.removeExhaustedPlayers();
+
+        game.displayPlayers();
+
+
     }
+
 
 }
